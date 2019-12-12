66cd9915f29e79161d76f7aae3752748536bc52b
gaoqiquan <g2994936517@163.com>
2019-12-12 17:47:09 +0800

    修改详情页

--- douban/miniprogram/pages/details/details.js
+++ douban/miniprogram/pages/details/details.js
@@ -12,6 +12,9 @@ Page({
     lineClamp: '5',
     menuType: true,
     menu: '展开更多...',
+    id: '',
+    show: false,
+    inputValue: ''
   },
 
   onClickMenu () {
@@ -27,7 +30,35 @@ Page({
       })
     }
   },
-
+  handlerClickPraise: function (e) {
+    let _name = e.currentTarget.dataset.id
+    let newDatas = []
+    this.data.getData.result.userEvaluateList.map(item => {
+      if (item.author === _name) {
+        if (item.type === '有用') {
+          item.number = parseInt(item.number) + 1
+          item.type = "无用"
+        } else {
+          item.number = parseInt(item.number) - 1
+          item.type = "有用"
+        }
+      }
+      newDatas.push(item)
+    })
+    db.collection('doubanbooks').doc(this.data.id).update({
+      data: {
+        ['result.userEvaluateList']: newDatas,
+      },
+      success: function (res) {
+        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+        console.log(res)
+      }
+    })
+    this.setData({
+      ['getData.result.userEvaluateList']: newDatas
+    })
+    console.log(newDatas)
+  },
   onClickUnfoldMore () {
     if (this.data.contentHidden === 'hidden') {
       this.setData({
@@ -44,15 +75,79 @@ Page({
     }
   },
 
+  getInputValue: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+
+  dateFtt (fmt, date) { //author: meizz 
+    let ret;
+    let opt = {
+      "Y+": date.getFullYear().toString(),        // 年
+      "m+": (date.getMonth() + 1).toString(),     // 月
+      "d+": date.getDate().toString(),            // 日
+      "H+": date.getHours().toString(),           // 时
+      "M+": date.getMinutes().toString(),         // 分
+      "S+": date.getSeconds().toString()          // 秒
+      // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+      ret = new RegExp("(" + k + ")").exec(fmt);
+      if (ret) {
+        fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+      };
+    };
+    return fmt;
+  },
+
+  onpublish: function () {
+    var crtTime = new Date()
+    let newDatas = []
+    let dataList = {
+      author: wx.getStorageSync('userInfo').nickName,
+      'data': this.dateFtt("YYYY-mm-dd HH:MM:SS", crtTime),
+      evaluate: '',
+      evaluateStore: this.data.inputValue,
+      number: 0,
+      type: '有用'
+    }
+    newDatas = this.data.getData
+    newDatas.result.userEvaluateList.push(dataList)
+    console.log(newDatas, this.data.id)
+    db.collection('doubanbooks').doc(this.data.id).update({
+      data: {
+        ['result.userEvaluateList']: newDatas,
+      }
+    }).then(res => {
+      console.log(res)
+    }).catch( err => {
+      console.log(err)
+    }
+    )
+    // this.setData({
+    //   ['getData.result.userEvaluateList']: newDatas
+    // })
+  },
+
   getDataList: function (id) {
     db.collection('doubanbooks').where({
       _id: id
     }).get().then(res => {
       this.setData({
-        getData: res.data[0]
+        getData: res.data[0],
+        id: id
       })
       console.log(this.data.getData)
     })
+  },
+
+  showPopup () {
+    this.setData({ show: true });
+  },
+
+  onClose() {
+    this.setData({ show: false });
   },
 
   /**
